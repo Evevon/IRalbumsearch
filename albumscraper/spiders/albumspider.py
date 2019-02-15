1c30b4a629461f2f11421e82965cc0cb5096d513
MakSaiWing <maksaiwing@MakSaiWings-MacBook-Pro.local>
2019-02-15 15:15:02 +0100

    album spider - gotten rid of the hard code

--- albumscraper/spiders/albumspider.py
+++ albumscraper/spiders/albumspider.py
@@ -8,16 +8,22 @@ class AlbumSpider(scrapy.Spider):
     name = 'album_spider'
     allowed_domains = ['pitchfork.com']
     # start urls are the urls for multiple infinite-scroll pages
-    start_urls = ['https://pitchfork.com/reviews/albums/?page='
-                    + str(n) for n in range(1, 1000)]
+    start_urls = ['https://pitchfork.com/reviews/albums/']
 
     def parse(self, response):
-         # collect all album links
-         albums = response.xpath('//div[@class="review"]/a/@href').extract()
-         # visit each album link and gather album info
-         for a in albums:
-             url = urljoin(response.url, a)
-             yield scrapy.Request(url, callback=self.parse_album)
+        # collect all album links
+        albums = response.xpath('//div[@class="review"]/a/@href').extract()
+        # visit each album link and gather album info
+        for a in albums:
+            url = urljoin(response.url, a)
+            yield scrapy.Request(url, callback=self.parse_album)
+
+        # follow pagination links
+        next_page = response.xpath(
+            "//link[contains(@data-react-helmet,'true') and contains(@rel,'next')]/@href").extract_first()
+        if next_page:
+            next_page_url = response.urljoin(next_page)
+            yield scrapy.Request(next_page_url,callback=self.parse)
 
     def parse_album(self, response):
         # extract info from html

5a7b8ff7f11c2fc2028417a1b9f6031eb007ae8a
Friso Selten <33626795+Fr1so@users.noreply.github.com>
2019-02-19 14:29:32 +0100

    Add files via upload

--- albumcrawling.py
+++ albumcrawling.py
@@ -1,9 +1,14 @@
 import scrapy
 from scrapy.crawler import CrawlerProcess
 from albumscraper.spiders import nme_spider, pitchfork_spider, rollingstone_spider
+from scrapy.settings import Settings
+import albumscraper.settings as my_settings
+
+crawler_settings = Settings()
+crawler_settings.setmodule(my_settings)
 
 # Distributed crawling of websites with spiders in parallel.
-process = CrawlerProcess()
+process = CrawlerProcess(settings=crawler_settings)
 process.crawl(nme_spider.NmeSpider)
 process.crawl(pitchfork_spider.PitchforkSpider)
 process.crawl(rollingstone_spider.RollingstoneSpider)

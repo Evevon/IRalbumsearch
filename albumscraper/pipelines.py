8cdf55db8d4ee2bedb4324e71f10e76380377292
Friso Selten <33626795+Fr1so@users.noreply.github.com>
2019-03-14 11:54:18 +0100

    Add files via upload

--- albumscraper/pipelines.py
+++ albumscraper/pipelines.py
@@ -7,10 +7,25 @@
 
 import preprocessing
 from jsonwriter import write_to_json
+import json
+import os
+from textblob import TextBlob
 
 
 class AlbumscraperPipeline(object):
     def process_item(self, album, spider):
+
+        dir_ = os.path.dirname(os.path.abspath(__file__))
+
+        #sentiment analysis
+        blob = album['description']
+        blob = TextBlob(blob)
+        sentiment_dict = {'sentiment': blob.sentiment[0], 'polarity': blob.sentiment[1]}
+
+
+        with open(dir_+ '/sentiment/' + spider.name + str(spider.count) + '_sentiment.json', 'w') as outfile:
+          json.dump(sentiment_dict, outfile)
+
         # text preprocessing
         album['description'] = preprocessing.preprocess_text(album['description'])
         album['name'] = preprocessing.preprocess_text(
